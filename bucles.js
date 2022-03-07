7a279b9d93b984d34332fd9d05df4d31a58c3ebd
kevynnolasco <kevynnolasco97@gmail.com>
2022-03-07 14:24:52 -0600

    first commit

---
+++ bucles.js
@@ -0,0 +1,16 @@
+for(let i = 0;i < 10;i++){
+         const m = i+1;
+        const result = number * m;
+   
+     console.log(` ${number}x ${m} = ${result}` ) ;
+
+    const number = i+1;
+    console.log(`tabla del ${number}`);
+
+    for(let i = 0;i < 10;i++){
+        const m = i+1;
+        const result = number * m;
+        //2 x 1 = 2 
+        console.log(`${number}x ${m} = ${result}` ) ;
+    } 
+}
